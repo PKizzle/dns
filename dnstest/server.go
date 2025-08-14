6686605db80ca630f7406e5f204da2ae7e44ac3c
Miek Gieben <miek@miek.nl>
2025-08-14 18:20:42 +0200

    Add dnstest package
    Signed-off-by: Miek Gieben <miek@miek.nl>

---
+++ dnstest/server.go
@@ -0,0 +1,119 @@
+package dnstest
+
+import (
+	"io"
+	"net"
+	"sync"
+	"time"
+
+	"codeberg.org/miekg/dns"
+)
+
+func Server(pc net.PacketConn, l net.Listener, opts ...func(*dns.Server)) (*dns.Server, string, chan error, error) {
+	server := &dns.Server{
+		PacketConn: pc,
+		Listener:   l,
+
+		ReadTimeout:  time.Hour,
+		WriteTimeout: time.Hour,
+	}
+
+	waitLock := sync.Mutex{}
+	waitLock.Lock()
+	server.NotifyStartedFunc = waitLock.Unlock
+
+	for _, opt := range opts {
+		opt(server)
+	}
+
+	var (
+		addr   string
+		closer io.Closer
+	)
+	if l != nil {
+		addr = l.Addr().String()
+		closer = l
+	} else {
+		addr = pc.LocalAddr().String()
+		closer = pc
+	}
+
+	// fin must be buffered so the goroutine below won't block
+	// forever if fin is never read from. This always happens
+	// if the channel is discarded and can happen in TestShutdownUDP.
+	fin := make(chan error, 1)
+
+	go func() {
+		fin <- server.ActivateAndServe()
+		closer.Close()
+	}()
+
+	waitLock.Lock()
+	return server, addr, fin, nil
+}
+
+func UDPServer(laddr string, opts ...func(*dns.Server)) (*dns.Server, string, chan error, error) {
+	pc, err := net.ListenPacket("udp", laddr)
+	if err != nil {
+		return nil, "", nil, err
+	}
+
+	return Server(pc, nil, opts...)
+}
+
+func PacketConnServer(laddr string, opts ...func(*dns.Server)) (*dns.Server, string, chan error, error) {
+	return UDPServer(laddr, append(opts, func(srv *dns.Server) {
+		// Make srv.PacketConn opaque to trigger the generic code paths.
+		srv.PacketConn = struct{ net.PacketConn }{srv.PacketConn}
+	})...)
+}
+
+/*
+func RunLocalTCPServer(laddr string, opts ...func(*dns.Server)) (*dns.Server, string, chan error, error) {
+	l, err := net.Listen("tcp", laddr)
+	if err != nil {
+		return nil, "", nil, err
+	}
+
+	return RunLocalServer(nil, l, opts...)
+}
+
+func RunLocalTLSServer(laddr string, config *tls.Config) (*dns.Server, string, chan error, error) {
+	return RunLocalTCPServer(laddr, func(srv *dns.Server) {
+		srv.Listener = tls.NewListener(srv.Listener, config)
+	})
+}
+
+func RunLocalUnixServer(laddr string, opts ...func(*dns.Server)) (*dns.Server, string, chan error, error) {
+	l, err := net.Listen("unix", laddr)
+	if err != nil {
+		return nil, "", nil, err
+	}
+
+	return RunLocalServer(nil, l, opts...)
+}
+
+func RunLocalUnixGramServer(laddr string, opts ...func(*dns.Server)) (*dns.Server, string, chan error, error) {
+	pc, err := net.ListenPacket("unixgram", laddr)
+	if err != nil {
+		return nil, "", nil, err
+	}
+
+	return RunLocalServer(pc, nil, opts...)
+}
+
+func RunLocalUnixSeqPacketServer(laddr string) (chan interface{}, string, error) {
+	pc, err := net.Listen("unixpacket", laddr)
+	if err != nil {
+		return nil, "", err
+	}
+
+	shutdownChan := make(chan interface{})
+	go func() {
+		pc.Accept()
+		<-shutdownChan
+	}()
+
+	return shutdownChan, pc.Addr().String(), nil
+}
+*/
