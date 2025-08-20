9e6e82a2e5d2e6ee6342d4a5b77ea4e492b24851
Miek Gieben <miek@miek.nl>
2025-08-20 12:46:20 +0200

    Add ci test with check for println
    Fixes: #50
    Signed-off-by: Miek Gieben <miek@miek.nl>

---
+++ ci_test.go
@@ -0,0 +1,50 @@
+package dns
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestPrintln(t *testing.T) {
+	files, _ := filepath.Glob("*.go")
+	subdirFiles, _ := filepath.Glob("*/*.go")
+	files = append(files, subdirFiles...)
+
+	for _, file := range files {
+		if strings.HasSuffix(file, "_test.go") {
+			continue
+		}
+
+		fset := token.NewFileSet()
+		node, err := parser.ParseFile(fset, file, nil, parser.ParseComments)
+		if err != nil {
+			t.Errorf("failed to parse file %s: %v", file, err)
+			continue
+		}
+
+		ast.Inspect(node, func(n ast.Node) bool {
+			call, ok := n.(*ast.CallExpr)
+			if !ok {
+				return true
+			}
+
+			// Check if it's an identifier (function call)
+			if ident, ok := call.Fun.(*ast.Ident); ok {
+				switch ident.Name {
+				case "println":
+					fallthrough
+				case "print":
+					pos := fset.Position(ident.Pos())
+					t.Errorf("%s:%d:%d: use of %s()", file, pos.Line, pos.Column, ident.Name)
+
+				default:
+				}
+			}
+			return true
+		})
+	}
+}
