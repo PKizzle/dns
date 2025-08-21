730928b79d4ef6b15ed68b661e607766b89658bb
Miek Gieben <miek@miek.nl>
2025-08-21 20:27:21 +0200

    another benchmark
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- bench_test.go
+++ bench_test.go
@@ -22,3 +22,20 @@ func BenchmarkPackName(b *testing.B) {
 		packName(name, buf, 0, nil, false)
 	}
 }
+
+func BenchmarkUnPackName(b *testing.B) {
+	m := &Msg{MsgHeader: MsgHeader{ID: 3, RecursionDesired: true}}
+	mx := &MX{Hdr: Header{Name: "miek.nl.", Class: ClassINET}}
+	m.Question = []RR{mx}
+	m.Pack()
+	// Output: [0 3 1 0 0 1 0 0 0 0 0 0 4 109 105 101 107 2 110 108 0 0 15 0 1]
+	name, _, err := UnpackName(m.Data, 12)
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.Logf("expected name: %s", name)
+
+	for b.Loop() {
+		UnpackName(m.Data, 12)
+	}
+}
