051ff698751c25fa975b2908fda3ee98b28018a1
Miek Gieben <miek@miek.nl>
2025-08-21 12:31:28 +0200

    Skeleton plugin server
    Signed-off-by: Miek Gieben <miek@miek.nl>

---
+++ cmd/testserv/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"runtime/pprof"
+	"syscall"
+
+	"codeberg.org/miekg/dns"
+)
+
+var (
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+	printf     = flag.Bool("print", false, "print replies")
+)
+
+const dom = "whoami.miek.nl."
+
+func serve(server *dns.Server, net string) {
+	server.Net = net
+	if err := server.ListenAndServe(); err != nil {
+		log.Printf("Failed to setup the "+net+" server: %s\n", err.Error())
+	}
+}
+
+func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	mux := dns.NewServeMux()
+
+	stack := []Plugin{
+		Log,
+		Whoami,
+	}
+
+	empty := func(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) { /*default mux does this*/ }
+	mux.HandleFunc("whoami.miek.nl.", Compile(empty, stack))
+
+	srv := &dns.Server{
+		Handler:       mux,
+		Addr:          "[::]:8054",
+		ReusePort:     true,
+		MaxTCPQueries: -1,
+	}
+
+	for range 10 {
+		go serve(srv, "tcp")
+		go serve(srv, "udp")
+	}
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	s := <-sig
+	fmt.Printf("Signal (%s) received, stopping\n", s)
+}
