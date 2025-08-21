2e8430a3f83f92c3211f2ce46b777b3b6cd01278
Miek Gieben <miek@miek.nl>
2025-08-21 07:55:40 +0200

    Add dnsutil.SetQuestion; was dns.SetQuestion
    Signed-off-by: Miek Gieben <miek@miek.nl>

---
+++ dnsutil/compat.go
@@ -0,0 +1,22 @@
+package dnsutil
+
+import "codeberg.org/miekg/dns"
+
+// SetQuestion set the question section in the message m.
+// It generates an ID and sets the RecursionDesired (RD) bit to true.
+// If the type t isn't know, nil is returned.
+func SetQuestion(m *dns.Msg, z string, t uint16) *dns.Msg {
+	m.ID = dns.ID()
+	m.RecursionDesired = true
+	var rr dns.RR
+	newFn, ok := dns.TypeToRR[t]
+	if !ok {
+		return nil
+	}
+	rr = newFn()
+	rr.Header().Name = z
+	rr.Header().Class = dns.ClassINET
+
+	m.Question = []dns.RR{rr}
+	return m
+}
