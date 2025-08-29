36332ff7c66a3ae6b2e375a67a1fe582aa246f03
Miek Gieben <miek@miek.nl>
2025-08-29 11:16:24 +0200

    trest
    Signed-off-by: Miek Gieben <miek@miek.nl>

---
+++ dnsutil/reverse_test.go
@@ -0,0 +1,29 @@
+package dnsutil
+
+import (
+	"bytes"
+	"net"
+	"testing"
+)
+
+func TestAddrReverse(t *testing.T) {
+	testcases := []struct {
+		reverse string
+		addr    net.IP
+	}{
+		{"54.119.58.176.in-addr.arpa.", net.ParseIP("176.58.119.54").To4()},
+		{".58.176.in-addr.arpa.", nil},
+		{"b.a.9.8.7.6.5.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.8.b.d.0.1.0.0.2.in-addr.arpa.", nil},
+		{"b.a.9.8.7.6.5.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.8.b.d.0.1.0.0.2.ip6.arpa.", net.ParseIP("2001:db8::567:89ab")},
+		{"d.0.1.0.0.2.ip6.arpa.", nil},
+		{"54.119.58.176.ip6.arpa.", nil},
+		{"NONAME", nil},
+		{"", nil},
+	}
+	for i, tc := range testcases {
+		got := AddrReverse(tc.reverse)
+		if bytes.Compare(got, tc.addr) != 0 {
+			t.Errorf("Test %d, expected '%s', got '%s'", i, tc.addr, got)
+		}
+	}
+}
