bbd5052d8daaeb96c99364fe4d9951a42fa5e496
Miek Gieben <miek@miek.nl>
2025-08-31 13:12:23 +0200

    Do the quick compare first
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- internal/pack/pack.go
+++ internal/pack/pack.go
@@ -220,7 +220,7 @@ loop:
 			// Don't try to compress '.'
 			// We should only compress when compress is true, but we should also still pick
 			// up names that can be used for *future* compression(s).
-			if !isRootLabel(s, bs, begin, ls) && compression != nil {
+			if compression != nil && !isRootLabel(s, bs, begin, ls) {
 				if p, ok := compression[s[compBegin:]]; ok {
 					// The first hit is the longest matching dname
 					// keep the pointer offset we get back and store
