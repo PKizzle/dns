829e779e1c5c483286c3ff2fc92557a6022b271b
Miek Gieben <miek@miek.nl>
2025-09-01 10:56:47 +0200

    Rewrite common_test
    Fixes: #107
    
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- dnsutil/common_test.go
+++ dnsutil/common_test.go
@@ -3,37 +3,24 @@ package dnsutil
 import "testing"
 
 func TestCommon(t *testing.T) {
-	s1 := "www.miek.nl."
-	s2 := "miek.nl."
-	s3 := "www.bla.nl."
-	s4 := "nl.www.bla."
-	s5 := "nl."
-	s6 := "miek.nl."
+	testcases := []struct {
+		a   string
+		b   string
+		exp int
+	}{
 
-	if Common(s1, s2) != 2 {
-		t.Errorf("%s with %s should be %d", s1, s2, 2)
+		{"www.miek.nl.", "miek.nl.", 2},
+		{"www.miek.nl.", "www.bla.nl.", 1},
+		{"www.bla..nl.", "ml.www.bla.", 0},
+		{"www.miek.nl.", "nl.", 1},
+		{"www.miek.nl.", "miek.nl.", 2},
+		{".", ".", 0},
+		{"example.org.", "EXAMPLE.ORG.", 2},
 	}
-	if Common(s1, s3) != 1 {
-		t.Errorf("%s with %s should be %d", s1, s3, 1)
-	}
-	if Common(s3, s4) != 0 {
-		t.Errorf("%s with %s should be %d", s3, s4, 0)
-	}
-	// Non qualified tests
-	if Common(s1, s5) != 1 {
-		t.Errorf("%s with %s should be %d", s1, s5, 1)
-	}
-	if Common(s1, s6) != 2 {
-		t.Errorf("%s with %s should be %d", s1, s5, 2)
-	}
-
-	if Common(s1, ".") != 0 {
-		t.Errorf("%s with %s should be %d", s1, s5, 0)
-	}
-	if x := Common(".", "."); x != 0 {
-		t.Errorf("%s with %s should be %d, got %d", ".", ".", 0, x)
-	}
-	if Common("test.com.", "TEST.COM.") != 2 {
-		t.Errorf("test.com. and TEST.COM. should be an exact match")
+	for _, tc := range testcases {
+		got := Common(tc.a, tc.b)
+		if got != tc.exp {
+			t.Errorf("expected %d, got %d, for %s/%s", tc.exp, got, tc.a, tc.b)
+		}
 	}
 }
