23b8370bdef9147b63c545ee8e895e0de0c9b654
Miek Gieben <miek@miek.nl>
2025-09-14 17:15:07 +0200

    Add resign go routine
    Signed-off-by: Miek Gieben <miek@miek.nl>

---
+++ cmd/testserv/handlers/sign/resign.go
@@ -0,0 +1,60 @@
+package sign
+
+import (
+	"path/filepath"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+// Resign launches a REsign routine that listens for _write_ events to the origin zone files and resigns them.
+func (s *Sign) Resign() error {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				log.Debug("Zone watch event", "file", filepath.Base(event.Name))
+				if !ok {
+					continue
+				}
+				switch {
+				case event.Has(fsnotify.Write):
+					fallthrough
+				case event.Has(fsnotify.Create):
+					fallthrough
+				case event.Has(fsnotify.Rename):
+					fallthrough
+				case event.Has(fsnotify.Remove):
+					// See comment in dbfile/reload.go
+					time.Sleep(2 * time.Second)
+
+					// check which zone needs resigning
+					for _, z := range s.Zones {
+						if z.Path == event.Name {
+							break
+						}
+					}
+				default:
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					continue
+				}
+				log.Debug("Zone watch event error", "err", err)
+			case <-s.ctx.Done():
+				watcher.Close()
+				return
+			}
+		}
+	}()
+
+	for _, z := range s.Zones {
+		watcher.Add(filepath.Dir(z.Path))
+	}
+	return nil
+}
