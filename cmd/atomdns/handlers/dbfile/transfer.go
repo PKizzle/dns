e999c4e2938e249df428bc981f17e20870e3e579
Miek Gieben <miek@miek.nl>
2025-09-16 08:26:26 +0200

    transfer.go: file that will contain all transfer logic
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- cmd/atomdns/handlers/dbfile/transfer.go
+++ cmd/atomdns/handlers/dbfile/transfer.go
@@ -1,1 +1,30 @@
 package dbfile
+
+import "codeberg.org/miekg/dns"
+
+// Transfer implements the transfer.Transfer interface.
+func (d *Dbfile) TransferOut() error {
+	// get soa and apex
+	apex, err := z.ApexIfDefined()
+	if err != nil {
+		return nil, err
+	}
+
+	ch := make(chan []dns.RR)
+	go func() {
+		if serial != 0 && apex[0].(*dns.SOA).Serial == serial { // ixfr fallback, only send SOA
+			ch <- []dns.RR{apex[0]}
+
+			close(ch)
+			return
+		}
+
+		ch <- apex
+		z.Walk(func(e *tree.Elem, _ map[uint16][]dns.RR) error { ch <- e.All(); return nil })
+		ch <- []dns.RR{apex[0]}
+
+		close(ch)
+	}()
+
+	return ch, nil
+}
