1c4d46e161357402e8cf26963c81c64a0f57a0e0
Miek Gieben <miek@miek.nl>
2025-09-19 09:41:49 +0200

    Add StringToMsg function in dnsutil
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- dnsutil/msg_test.go
+++ dnsutil/msg_test.go
@@ -1,36 +1,61 @@
 package dnsutil
 
 import (
+	"strings"
 	"testing"
+	"unicode"
+
+	"codeberg.org/miekg/dns"
+	"codeberg.org/miekg/dns/dnstest"
 )
 
 func TestStringToMsg(t *testing.T) {
 	testcases := []struct {
-		in  string
-		out string
+		in  func() *dns.Msg
+		exp string
 	}{
 		{
-			in: `
+			in: func() *dns.Msg {
+				m := dns.NewMsg("miek.nl.", dns.TypeMX)
+				m.ID = 49123
+				m.Response, m.RecursionDesired, m.RecursionAvailable = true, true, true
+				m.Answer = []dns.RR{
+					dnstest.New("miek.nl. 11381 IN MX 10 aspmx2.googlemail.com."),
+					dnstest.New("miek.nl. 11381 IN MX 1 aspmx.l.google.com."),
+				}
+				return m
+			},
+			exp: `
 ;; QUERY, rcode: NOERROR, id: 49123, flags: qr rd ra
-;; QUESTION: 1, PSEUDO: 0, ANSWER: 5, AUTHORITY: 0, ADDITIONAL: 0, DATA SIZE: 0
+;; QUESTION: 1, PSEUDO: 0, ANSWER: 2, AUTHORITY: 0, ADDITIONAL: 0, DATA SIZE: 0
 
 ;; QUESTION SECTION:
 miek.nl.                IN      MX
 
 ;; ANSWER SECTION:
 miek.nl.        11381   IN      MX      10 aspmx2.googlemail.com.
-miek.nl.        11381   IN      MX      10 aspmx3.googlemail.com.
-miek.nl.        11381   IN      MX      5 alt1.aspmx.l.google.com.
-miek.nl.        11381   IN      MX      5 alt2.aspmx.l.google.com.
-miek.nl.        11381   IN      MX      1 aspmx.l.google.com.`,
-			out: "",
+miek.nl.        11381   IN      MX      1 aspmx.l.google.com.
+`,
 		},
 	}
 	for _, tc := range testcases {
-		m, err := StringToMsg(tc.in)
+		s := tc.in().String()
+		m, err := StringToMsg(s)
 		if err != nil {
 			t.Error(err)
 		}
-		println(m.String())
+		if trim(m.String()) != trim(tc.exp) {
+			t.Logf("%s\n%s", m.String(), tc.exp)
+			t.Errorf("string representations do not match")
+		}
 	}
+}
+
+func trim(s string) string {
+	return strings.Map(func(r rune) rune {
+		if unicode.IsSpace(r) {
+			return -1
+		}
+		return r
+	}, s)
 }
