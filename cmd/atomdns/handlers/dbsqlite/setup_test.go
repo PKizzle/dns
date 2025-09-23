107f38ff30ec258bd75bd650c4b24a2f9cd246e0
Miek Gieben <miek@miek.nl>
2025-09-23 16:37:29 +0200

    more
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- cmd/atomdns/handlers/dbsqlite/setup_test.go
+++ cmd/atomdns/handlers/dbsqlite/setup_test.go
@@ -1,1 +1,87 @@
 package dbsqlite
+
+import (
+	"slices"
+	"testing"
+
+	"codeberg.org/miekg/dns/cmd/atomdns/internal/dnsserver"
+)
+
+func TestSetup(t *testing.T) {
+	testcases := []struct {
+		input string
+		exp   *Dbsqlite
+	}{
+		{`dbsqlite db.example`, &Dbsqlite{Path: "db.example"}},
+		{
+			`dbsqlite db.example {
+                        transfer {
+                                to {
+                                        notify 10.240.1.1
+                                }
+                        }
+                }`,
+			&dbsqlite{Path: "db.example", To: &Transfer{IPs: []string{}, Notifies: []string{"10.240.1.1"}}},
+		},
+		{
+			`dbsqlite db.example {
+                        transfer {
+                                to 172.16.16.1 {
+                                        notify 10.240.1.1
+                                }
+                        }
+                }`,
+			&dbsqlite{Path: "db.example", To: &Transfer{IPs: []string{"172.16.16.1"}, Notifies: []string{"10.240.1.1"}}},
+		},
+		{
+			`dbsqlite db.example {
+                        transfer {
+                                to 172.16.16.1 {
+                                        notify 10.240.1.1
+                                        key miek.nl hmac-sha224 aGFsbG8K
+                                        source 10.10.10.10
+                                }
+                                from 244.22.21.10
+                        }
+                }`,
+			&dbsqlite{
+				Path: "db.example",
+				To: &Transfer{
+					IPs:        []string{"172.16.16.1"},
+					Sources:    []string{"10.10.10.10"},
+					Notifies:   []string{"10.240.1.1"},
+					TSIGSecret: "aGFsbG8K",
+				},
+				From: &Transfer{
+					IPs: []string{"244.22.21.10"},
+				},
+			},
+		},
+	}
+	for i, tc := range testcases {
+		dbsqlite := new(Dbsqlite)
+		co := dnsserver.NewTestController(tc.input)
+		err := dbsqlite.Setup(co)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if tc.exp.Path != dbsqlite.Path {
+			t.Errorf("test %d: expected %s, got %s", i, tc.exp.Path, dbsqlite.Path)
+		}
+		if tc.exp.To != nil {
+			if slices.Compare(tc.exp.To.IPs, dbsqlite.To.IPs) != 0 {
+				t.Errorf("test %d: expected %v, got %v", i, tc.exp.To.IPs, dbsqlite.To.IPs)
+			}
+			if slices.Compare(tc.exp.To.Notifies, dbsqlite.To.Notifies) != 0 {
+				t.Errorf("test %d: expected %v, got %v", i, tc.exp.To.Notifies, dbsqlite.To.Notifies)
+			}
+			if slices.Compare(tc.exp.To.Sources, dbsqlite.To.Sources) != 0 {
+				t.Errorf("test %d: expected %v, got %v", i, tc.exp.To.Sources, dbsqlite.To.Sources)
+			}
+			if tc.exp.To.TSIGSecret != dbsqlite.To.TSIGSecret {
+				t.Errorf("test %d: expected %s, got %s", i, tc.exp.To.TSIGSecret, dbsqlite.To.TSIGSecret)
+			}
+		}
+	}
+}
