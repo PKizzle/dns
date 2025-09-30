f12e80d51ddd57f4be02a75b531a864a1e7c24d6
Miek Gieben <miek@miek.nl>
2025-09-30 14:32:27 +0200

    Remove println
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- cmd/atomdns/handlers/unpack/unpack.go
+++ cmd/atomdns/handlers/unpack/unpack.go
@@ -11,7 +11,6 @@ type Unpack int
 func (u *Unpack) HandlerFunc(next dns.HandlerFunc) dns.HandlerFunc {
 	return dns.HandlerFunc(func(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) {
 		if err := r.Unpack(); err != nil {
-			println(len(r.Data))
 			log.Debug("Unpack failure", Err(err))
 		}
 		next.ServeDNS(ctx, w, r)
