a1ee3a643dc9f26c54c20dcdb4ad769b99d35163
Miek Gieben <miek@miek.nl>
2025-10-05 18:42:35 +0200

    Release 0.5.8

--- version.go
+++ version.go
@@ -3,7 +3,7 @@ package dns
 import "fmt"
 
 // Version is current version of this library.
-var Version = v{0, 5, 7}
+var Version = v{0, 5, 8}
 
 // v holds the version of this library.
 type v struct {
