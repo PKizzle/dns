2b360838c405c8f98cfa01f6f8961851f75bc500
Miek Gieben <miek@miek.nl>
2025-10-06 07:29:03 +0200

    atomdns: structured query logging
    @cl: use slog with fields to log the query: example.org. remote=::1 port=53806 id=61098 type=MX class=IN name=example.org. network=udp size=52 bufsize=1232 opcode=QUERY
    
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- cmd/atomdns/handlers/log/README.md
+++ cmd/atomdns/handlers/log/README.md
@@ -22,18 +22,18 @@ log
 A typical example looks like this:
 
 ```txt
-2025/09/13 07:36:41 INFO ::1 :58588 - 50123 "TXT IN bla.example.org. udp 56 1232" rd ad QUERY
+2025/10/06 07:25:52 INFO example.org. remote=::1 port=40689 id=23343 type=MX class=IN name=example.ORG. network=udp size=52 bufsize=1232 opcode=QUERY
 ```
 
 Which says:
 
-- Remote address and port: `::1 :58588`.
-- Query ID `50123`.
-- Question type, question class, question name: `TXT IN bla.example.org.`.
+- Zone getting the request: `example.org.`.
+- Remote address and port: `::1 40689`.
+- Query ID `23343`.
+- Question type, question class, incoming question name: `MX IN example.ORG.`.
 - Network: `udp`.
-- Size in bytes: `56`.
+- Size in bytes: `52`.
 - Advertised UDP buffer: `1232`.
-- Header flags: `rd ad`.
 - Opcode: `QUERY`.
 
 # Also See
--- cmd/atomdns/handlers/log/log.go
+++ cmd/atomdns/handlers/log/log.go
@@ -1,12 +1,8 @@
 package log
 
 import (
-	"bytes"
 	"context"
 	"log/slog"
-	"strconv"
-	"sync"
-	"text/template"
 
 	"codeberg.org/miekg/dns"
 	"codeberg.org/miekg/dns/dnsutil"
@@ -22,94 +18,25 @@ func (l *Log) HandlerFunc(next dns.HandlerFunc) dns.HandlerFunc {
 			next.ServeDNS(ctx, w, r)
 			return
 		}
-		b := bufPool.Get().(*bytes.Buffer)
 
-		logtmpl.Execute(b, logWrap{w, r})
-		logger.Info(b.String())
+		logger.
+			With("remote", dnsutil.RemoteIP(w)).
+			With("port", dnsutil.RemotePort(w)).
+			With(slog.Int("id", int(r.ID))).
+			With("type", func() string { _, t := dnsutil.Question(r); return dnsutil.TypeToString(t) }()).
+			With("class", dnsutil.ClassToString(r.Question[0].Header().Class)).
+			With("name", func() string { z, _ := dnsutil.Question(r); return z }()).
+			With("network", dnsutil.Network(w)).
+			With(slog.Int("size", len(r.Data))).
+			With(slog.Int("bufsize", func() int {
+				if r.UDPSize < 512 {
+					return 512
+				}
+				return int(r.UDPSize)
+			}())).
+			With("opcode", dnsutil.OpcodeToString(r.Opcode)).
+			Info(dns.Zone(ctx))
 
-		b.Reset()
-		bufPool.Put(b)
 		next.ServeDNS(ctx, w, r)
 	})
-}
-
-type logWrap struct {
-	dns.ResponseWriter
-	*dns.Msg
-}
-
-var funcmap = template.FuncMap{
-	"id":     func(l logWrap) string { return strconv.Itoa(int(l.Msg.ID)) },
-	"opcode": func(l logWrap) string { return dnsutil.OpcodeToString(l.Msg.Opcode) },
-	"bufsize": func(l logWrap) string {
-		if l.Msg.UDPSize < 512 {
-			return "512"
-		}
-		return strconv.Itoa(int(l.Msg.UDPSize))
-	},
-	"flags": func(l logWrap) string { return flags(l) },
-	"size":  func(l logWrap) string { return strconv.Itoa(len(l.Msg.Data)) },
-	"type": func(l logWrap) string {
-		_, t := dnsutil.Question(l.Msg)
-		return dnsutil.TypeToString(t)
-	},
-	"name":    func(l logWrap) string { z, _ := dnsutil.Question(l.Msg); return z },
-	"class":   func(l logWrap) string { return dnsutil.ClassToString(l.Msg.Question[0].Header().Class) },
-	"network": func(l logWrap) string { return dnsutil.Network(l.ResponseWriter) },
-	"remote":  func(l logWrap) string { return dnsutil.RemoteIP(l.ResponseWriter) },
-	"port":    func(l logWrap) string { return dnsutil.RemotePort(l.ResponseWriter) },
-	"local":   func(l logWrap) string { return dnsutil.LocalIP(l.ResponseWriter) },
-}
-
-var logtmpl = template.Must(template.New("logFunc").Funcs(funcmap).Parse(line))
-
-const line = `{{remote .}} :{{port .}} - {{id .}} "{{type .}} {{class .}} {{name .}} {{network .}} {{size .}} {{bufsize .}}" {{flags .}} {{opcode .}}`
-
-var bufPool = &sync.Pool{
-	New: func() any {
-		return new(bytes.Buffer)
-	},
-}
-
-func flags(l logWrap) string {
-	h := l.Msg.MsgHeader
-	b := bufPool.Get().(*bytes.Buffer)
-	defer func() {
-		b.Reset()
-		bufPool.Put(b)
-	}()
-	if h.Response {
-		b.WriteString(" qr")
-	}
-	if h.Authoritative {
-		b.WriteString(" aa")
-	}
-	if h.Truncated {
-		b.WriteString(" tc")
-	}
-	if h.RecursionDesired {
-		b.WriteString(" rd")
-	}
-	if h.RecursionAvailable {
-		b.WriteString(" ra")
-	}
-	if h.Zero {
-		b.WriteString(" z")
-	}
-	if h.AuthenticatedData {
-		b.WriteString(" ad")
-	}
-	if h.CheckingDisabled {
-		b.WriteString(" cd")
-	}
-	if h.Security {
-		b.WriteString(" do")
-	}
-	if h.CompactAnswers {
-		b.WriteString(" co")
-	}
-	if h.Delegation {
-		b.WriteString(" de")
-	}
-	return string(b.Bytes()[1:])
 }
