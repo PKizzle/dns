89a03de7880508e43bb85f265aeed812431b5d6a
Miek Gieben <miek@miek.nl>
2025-10-15 15:16:43 +0200

    More
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- rr_test.go
+++ rr_test.go
@@ -16,25 +16,24 @@ type YO struct {
 	Yo       string `dns:"txt"`
 }
 
-// Typer interface
-func (rr *YO) Type() uint16 { return 65281 }
+const codepoint = 65281
 
-// RR interface
+// Typer interface.
+func (rr *YO) Type() uint16 { return codepoint }
+
+// RR interface.
 func (rr *YO) Header() *dns.Header { return &rr.Hdr }
 func (rr *YO) Len() int            { return rr.Hdr.Len() + 2 + len(rr.Yo) + 1 }
 func (rr *YO) String() string      { return dnsfmt.Header(rr) + fmt.Sprintf("\t%d %s", rr.Priority, rr.Yo) }
 func (rr *YO) Clone() dns.RR       { return &YO{rr.Hdr, rr.Priority, rr.Yo} }
 
+// Parser interface.
 func (rr *YO) Parse(o string, tokens []string) error {
-	if len(tokens) < 2 {
-		// no rdata
+	if len(tokens) < 2 { // no rdata
 		return nil
 	}
 	i, err := strconv.ParseUint(tokens[0], 10, 32)
-	if err != nil {
-		return err
-	}
-	if i > 255 {
+	if err != nil || i > 255 {
 		return fmt.Errorf("bad YO Priority")
 	}
 	rr.Priority = uint8(i)
@@ -43,15 +42,17 @@ func (rr *YO) Parse(o string, tokens []string) error {
 }
 
 func TestExternalRR(t *testing.T) {
-	dns.TypeToRR[65281] = func() dns.RR { return new(YO) }
-	dns.TypeToString[65281] = "YO"
-	dns.StringToType["YO"] = 65281
+	dns.TypeToRR[codepoint] = func() dns.RR { return new(YO) }
+	dns.TypeToString[codepoint] = "YO"
+	dns.StringToType["YO"] = codepoint
 
 	y := &YO{Hdr: dns.Header{Name: "example.org.", Class: dns.ClassINET}, Priority: 10, Yo: "Yo!"}
-	println(y.String())
 	rry, err := dns.New(y.String())
 	if err != nil {
 		t.Fatal(err)
+	}
+	if rry.String() != y.String() {
+		t.Fatal("YO string presentations should be identical")
 	}
 	fmt.Println(rry.String())
 }
