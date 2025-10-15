377962ffe3293bbd2b38325905ea79943609da06
Miek Gieben <miek@miek.nl>
2025-10-15 15:32:54 +0200

    condense this a bit
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- dnsfmt/dnsfmt.go
+++ dnsfmt/dnsfmt.go
@@ -7,7 +7,6 @@ package dnsfmt
 import (
 	"strconv"
 	"strings"
-	"sync"
 
 	"codeberg.org/miekg/dns"
 	"codeberg.org/miekg/dns/dnsutil"
@@ -16,50 +15,36 @@ import (
 
 // Header returns the header of the RR as a formatted string.
 func Header(rr dns.RR) string {
-	sb := builderPool.Get()
-
+	sb := strings.Builder{}
 	sb.WriteString(Name(rr.Header().Name))
 	sb.WriteByte('\t')
-
 	sb.WriteString(strconv.FormatInt(int64(rr.Header().TTL), 10))
 	sb.WriteByte('\t')
-
 	sb.WriteString(dnsutil.ClassToString(rr.Header().Class))
 	sb.WriteByte('\t')
-
 	rrtype := dns.RRToType(rr)
 	sb.WriteString(dnsutil.TypeToString(rrtype))
 	sb.WriteByte('\t')
-	s := sb.String()
-	builderPool.Put(sb)
-	return s
+	return sb.String()
 }
 
 // OptionHeader returns the header of the EDNS0 RR as a formatted string.
 func OptionHeader(rr dns.EDNS0) string {
-	sb := builderPool.Get()
-
+	sb := strings.Builder{}
 	sb.WriteByte('.')
 	sb.WriteByte('\t')
-
 	sb.WriteByte('\t') // skip TTL
-
 	sb.WriteString(dnsutil.ClassToString(rr.Header().Class))
 	sb.WriteByte('\t')
-
 	code := dns.RRToCode(rr)
 	sb.WriteString(dnsutil.CodeToString(code))
 	sb.WriteByte('\t')
-	s := sb.String()
-	builderPool.Put(sb)
-	return s
+	return sb.String()
 }
 
 // Name returns the string format of a domain name.
 func Name(s string) string {
-	sb := builderPool.Get()
-	defer builderPool.Put(sb)
-
+	sb := strings.Builder{}
 	for i := 0; i < len(s); {
 		if s[i] == '.' {
 			if sb.Len() != 0 {
@@ -102,13 +87,3 @@ func Name(s string) string {
 	}
 	return sb.String()
 }
-
-// builderPooler is a pool used by the String methods.
-type builderPooler struct {
-	sync.Pool
-}
-
-func (s *builderPooler) Get() strings.Builder   { return s.Pool.Get().(strings.Builder) }
-func (s *builderPooler) Put(sb strings.Builder) { sb.Reset(); s.Pool.Put(sb) }
-
-var builderPool = &builderPooler{Pool: sync.Pool{New: func() any { return strings.Builder{} }}}
