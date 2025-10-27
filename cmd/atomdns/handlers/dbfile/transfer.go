bc28e2b7f2217d20232c541af40777506c2b50ea
Miek Gieben <miek@miek.nl>
2025-10-27 14:04:52 +0100

    compile
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- cmd/atomdns/handlers/dbfile/transfer.go
+++ cmd/atomdns/handlers/dbfile/transfer.go
@@ -28,7 +28,7 @@ func (d *Dbfile) HandlerFuncTransfer(ctx context.Context, w dns.ResponseWriter, 
 		log.Debug("Failure to transfer out", Err(err))
 		return
 	}
-	alog := log.With(slog.String("zone", z.Origin()), slog.String("path", filepath.Base(d.Path)), slog.String("upstream", w.RemoteAddr()).String())
+	alog := log.With(slog.String("zone", z.Origin()), slog.String("path", filepath.Base(d.Path)), slog.Any("upstream", w.RemoteAddr()))
 	alog.Info("Successful transfer out")
 }
 
