b7474386557edf6d9262f380d1befd859adad0f0
Miek Gieben <miek@miek.nl>
2025-10-28 10:03:10 +0100

    remove ninfo from test as well
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- cmd/atomdns/handlers/sign/sign_test.go
+++ cmd/atomdns/handlers/sign/sign_test.go
@@ -65,7 +65,7 @@ func TestSign(t *testing.T) {
 			func() *dnszone.Node { return &dnszone.Node{} },
 			func(a, b *dnszone.Node) error {
 				for _, rr := range a.RRs {
-					exp := []uint16{dns.TypeNS, dns.TypeSOA, dns.TypeMX, dns.TypeAAAA, dns.TypeRRSIG, dns.TypeNSEC, dns.TypeDNSKEY, dns.TypeNINFO, dns.TypeCDS, dns.TypeCDNSKEY}
+					exp := []uint16{dns.TypeNS, dns.TypeSOA, dns.TypeMX, dns.TypeAAAA, dns.TypeRRSIG, dns.TypeNSEC, dns.TypeDNSKEY, dns.TypeCDS, dns.TypeCDNSKEY}
 					if n, ok := rr.(*dns.NSEC); ok {
 						if slices.Compare(n.TypeBitMap, exp) != 0 {
 							return fmt.Errorf("type bitmap is not: %v != %v", exp, n.TypeBitMap)
