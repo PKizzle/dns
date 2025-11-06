3a5644642d32508940b0f1fb1bacd659c51ded6b
Miek Gieben <miek@miek.nl>
2025-11-06 16:22:06 +0100

    better naming
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- dnsutil/nsec3.go
+++ dnsutil/nsec3.go
@@ -9,21 +9,25 @@ import (
 )
 
 // NSEC3Name returns the hashed label according to RFC 5155.
-func NSEC3Name(label, salt string, iter uint16) string {
+func NSEC3Name(name, salt string, iter uint16) string {
 	hashdata := make([]byte, hex.DecodedLen(len(salt))+255)
 	n, err := pack.StringHex(salt, hashdata, 0)
 	if err != nil {
 		return ""
 	}
-	m, err := pack.Name(label, hashdata[n:], 0, nil, false)
+	m, err := pack.Name(name, hashdata[n:], 0, nil, false)
 	if err != nil {
 		return ""
 	}
 	hashdata = hashdata[:n+m]
 
 	s := sha1.New()
-	var nsec3 []byte
-	for k := uint16(0); k < iter+1; k++ {
+	// k = 0
+	s.Write(hashdata)
+	nsec3 := s.Sum(nil)
+
+	// k > 0
+	for k := uint16(0); k < iter; k++ {
 		s.Reset()
 		s.Write(hashdata)
 		nsec3 = s.Sum(nil)
