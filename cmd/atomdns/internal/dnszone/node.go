93dd1076543163ecdb5f95e1dc750ef1aa3b4bfb
Miek Gieben <miek@miek.nl>
2025-11-12 14:23:39 +0100

    Make less faster
    Inspired by deae7ec3455ec11fc4ddf89f868507f562cc40d3 from
    coredns/coredns.
    
    As we sort in the main lib we only need to fix this to the benefit of
    all callers.
    
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- cmd/atomdns/internal/dnszone/node.go
+++ cmd/atomdns/internal/dnszone/node.go
@@ -31,7 +31,4 @@ func (n *Node) String() string {
 }
 
 // Less compares nodes a, b by Name and returns true if a is less than b.
-func Less(a, b *Node) bool {
-	x := dns.CompareName(a.Name, b.Name)
-	return x == -1
-}
+func Less(a, b *Node) bool { x := dns.CompareName(a.Name, b.Name); return x == -1 }
