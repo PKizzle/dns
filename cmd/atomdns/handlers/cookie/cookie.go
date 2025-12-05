10912ea0c325da5bd0f87e341a736c9b5e383b54
Miek Gieben <miek@miek.nl>
2025-12-05 09:39:19 +0100

    atomdns: Use dnsctx.Key() everywhere
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- cmd/atomdns/handlers/cookie/cookie.go
+++ cmd/atomdns/handlers/cookie/cookie.go
@@ -42,7 +42,7 @@ func (c *Cookie) HandlerFunc(next dns.HandlerFunc) dns.HandlerFunc {
 						return m
 					})
 
-				ctx = dnsctx.WithValue(ctx, c.Key()+dnsctx.Status, true)
+				ctx = dnsctx.WithValue(ctx, dnsctx.Key(c.Key(), dnsctx.Status), true)
 				break
 			}
 		}
