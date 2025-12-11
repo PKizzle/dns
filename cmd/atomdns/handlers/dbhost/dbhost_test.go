5554577db50be426088f1282ab0e6c13ef405c22
Aaron Gable <aaron@letsencrypt.org>
2025-12-11 08:54:10 -0800

    Use == instead of .Compare

--- cmd/atomdns/handlers/dbhost/dbhost_test.go
+++ cmd/atomdns/handlers/dbhost/dbhost_test.go
@@ -22,7 +22,7 @@ func TestDbhost(t *testing.T) {
 	next := new(whoami.Whoami).HandlerFunc(nil)
 	h.HandlerFunc(next).ServeDNS(context.TODO(), w, r)
 
-	if x := w.Msg.Answer[0].(*dns.A).A; x.Compare(netip.MustParseAddr("127.0.0.1")) != 0 {
+	if x := w.Msg.Answer[0].(*dns.A).A; x != netip.MustParseAddr("127.0.0.1") {
 		t.Fatalf("expected %s, got %s", "127.0.0.1", x)
 	}
 }
