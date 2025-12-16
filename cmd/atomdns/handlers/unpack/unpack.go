328a6e741b1bcf348d736b1171b852365fb7f5d0
Miek Gieben <miek@miek.nl>
2025-12-16 19:38:21 +0100

    better unpack; show zone; should also show msg
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- cmd/atomdns/handlers/unpack/unpack.go
+++ cmd/atomdns/handlers/unpack/unpack.go
@@ -11,7 +11,7 @@ type Unpack int
 func (u *Unpack) HandlerFunc(next dns.HandlerFunc) dns.HandlerFunc {
 	return dns.HandlerFunc(func(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) {
 		if err := r.Unpack(); err != nil {
-			log().Debug("Unpack failure", Err(err))
+			log().Debug("Unpack failure", Err(err), "zone", dns.Zone(ctx))
 		}
 		next.ServeDNS(ctx, w, r)
 	})
