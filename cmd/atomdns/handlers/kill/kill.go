eece5cd1e7e3e7daa147660d0090b0155379572a
Miek Gieben <miek@miek.nl>
2025-12-18 15:36:45 +0100

    atomdns, kill: add kill handler
    @cl: atomdns: add kill handler
    
    Signed-off-by: Miek Gieben <miek@miek.nl>

---
+++ cmd/atomdns/handlers/kill/kill.go
@@ -0,0 +1,13 @@
+package kill
+
+import (
+	"context"
+
+	"codeberg.org/miekg/dns"
+)
+
+type Kill int
+
+func (k *Kill) HandlerFunc(next dns.HandlerFunc) dns.HandlerFunc {
+	return dns.HandlerFunc(func(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) { next.ServeDNS(ctx, w, r) })
+}
