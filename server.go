92916fcfa9a0b3862990db99789c501f908e7468
Florian Maury <pub-codeberg@x-cli.eu>
2025-12-19 18:05:01 +0100

    fix: Server.Net useless when specifying a listener
    Closes #514
    
    Server.Net only needs to be specified when Listener or PacketConn are
    nil. Having the use of Listener/PacketConn in the switch unnecessarily
    required specifying Net.
    
    Signed-off-by: Florian Maury <pub-codeberg@x-cli.eu>

--- server.go
+++ server.go
@@ -171,12 +171,17 @@ func (srv *Server) ListenAndServe() error {
 	}
 	srv.init()
 
+	if srv.Listener != nil {
+		srv.listenTCP(srv.Listener)
+		return nil
+	}
+	if srv.PacketConn != nil {
+		srv.listenUDP(srv.PacketConn)
+		return nil
+	}
+
 	switch srv.Net {
 	case "tcp", "tcp4", "tcp6":
-		if srv.Listener != nil {
-			srv.listenTCP(srv.Listener)
-			return nil
-		}
 		l, err := listenTCP(srv.Net, addr, srv.ReusePort, srv.ReuseAddr)
 		if err != nil {
 			return err
@@ -191,10 +196,6 @@ func (srv *Server) ListenAndServe() error {
 		srv.listenTCP(l)
 		return nil
 	case "udp", "udp4", "udp6":
-		if srv.PacketConn != nil {
-			srv.listenUDP(srv.PacketConn)
-			return nil
-		}
 		l, err := listenUDP(srv.Net, addr, srv.ReusePort, srv.ReuseAddr)
 		if err != nil {
 			return err
