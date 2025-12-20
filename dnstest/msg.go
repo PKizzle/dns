a7a9187775f899b8a0c1277b2b7052aff6c9f93d
Miek Gieben <miek@miek.nl>
2025-12-20 11:48:05 +0100

    Add dnstest.NewMsg for making tests slightly shorter
    Signed-off-by: Miek Gieben <miek@miek.nl>

---
+++ dnstest/msg.go
@@ -0,0 +1,12 @@
+package dnstest
+
+import "codeberg.org/miekg/dns"
+
+// NewMsg returns a test message with an ID of 3, the question set to "www.example.org."/A. The message is
+// packed before it is returned.
+func NewMsg() *dns.Msg {
+	m := dns.NewMsg("a.example.org.", dns.TypeA)
+	m.ID = 3
+	m.Pack()
+	return m
+}
