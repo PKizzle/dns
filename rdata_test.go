7ec3b3d236cc9cc3f614620773164004935e6dee
Miek Gieben <miek@miek.nl>
2026-01-28 22:08:16 +0100

    Fix tests
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- rdata_test.go
+++ rdata_test.go
@@ -11,6 +11,7 @@ import (
 // Example on how get the text presentation of an [dns.RR].
 func ExampleRDATA_string() {
 	rr := &dns.MX{Hdr: dns.Header{Name: "miek.nl.", Class: dns.ClassINET, TTL: 3600}, MX: rdata.MX{Preference: 10, Mx: "mx.miek.nl."}}
-	s := rr.Header().String() + " " + dnsutil.TypeToString(dns.RRToType(rr)) + "\t" + rr.MX.String()
+	s := rr.Header().String() + " " + dnsutil.TypeToString(dns.RRToType(rr)) + "\t" + rr.Data().String()
 	fmt.Println(s)
+	// Output: miek.nl.	3600	IN MX	10 mx.miek.nl.
 }
