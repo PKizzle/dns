adf33e0e81795f59619b45ee0804a5b6769da257
Miek Gieben <miek@miek.nl>
2026-02-03 07:38:00 +0100

    Add parse to temporary
    Signed-off-by: Miek Gieben <miek@miek.nl>

---
+++ cmd/parse/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"runtime/pprof"
+	"runtime/trace"
+
+	"codeberg.org/miekg/dns"
+)
+
+var (
+	flagcpu   = flag.Bool("cpu", false, "write cpu profile to cpu.out")
+	flagtrace = flag.Bool("trace", false, "write trace profile to trace.out")
+	flagprint = flag.Bool("print", false, "write each RR to standard output")
+)
+
+func main() {
+	flag.Parse()
+
+	if *flagtrace {
+		f, err := os.Create("trace.out")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		trace.Start(f)
+		defer trace.Stop()
+	}
+
+	if *flagcpu {
+		f, err := os.Create("cpu.out")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	z, err := os.Open(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
+	zp := dns.NewZoneParser(z, ".", flag.Arg(0))
+	i := 0
+	for rr, ok := zp.Next(); ok; rr, ok = zp.Next() {
+		if *flagprint {
+			fmt.Println(rr.String())
+		}
+		i++
+	}
+	if zp.Err() != nil {
+		log.Fatal(zp.Err())
+	}
+	fmt.Println(i)
+}
