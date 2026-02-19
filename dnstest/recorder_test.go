2f4b3499f693bdbf8cb0cd085b9eb8275b1724ed
Miek Gieben <miek@miek.nl>
2026-02-19 20:22:18 +0100

    dnstest: add MultiRecorder
    Useful in coredns, so useful in general, add to dnstest.
    
    Fixes: #718
    
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- dnstest/recorder_test.go
+++ dnstest/recorder_test.go
@@ -18,3 +18,19 @@ func TestRecorder(t *testing.T) {
 		t.Errorf("expected %s, got %s", "miek.nl.", x)
 	}
 }
+
+func TestMultiRecorder(t *testing.T) {
+	m := new(dns.Msg)
+	m.Question = []dns.RR{&dns.TXT{Hdr: dns.Header{Name: "miek.nl.", Class: dns.ClassINET}}}
+
+	rec := NewMultiRecorder(nil)
+	io.Copy(rec, m)
+	io.Copy(rec, m)
+	if len(rec.Msgs) != 2 {
+		t.Errorf("expeced 2 messages, got %d", len(rec.Msgs))
+	}
+	rec.Msg.Unpack()
+	if x := rec.Msg.Question[0].Header().Name; x != "miek.nl." {
+		t.Errorf("expected %s, got %s", "miek.nl.", x)
+	}
+}
