c51f8378d19c13fb14c4231daf4941e17364e315
Miek Gieben <miek@miek.nl>
2026-02-21 15:40:49 +0100

    Documentation updates
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- dnsutil/string.go
+++ dnsutil/string.go
@@ -7,6 +7,7 @@ import (
 )
 
 // TypeToString converts the type to the text presentation, or to "TYPE"+value if the type is unknown.
+// Also see [ClassToString], [RcodeToString], [OpcodeToString], [CodeToString] for similar functions.
 func TypeToString(t uint16) string {
 	if t1, ok := dns.TypeToString[t]; ok {
 		return t1
@@ -15,6 +16,7 @@ func TypeToString(t uint16) string {
 }
 
 // RcodeToString converts the code to the text presentation, or to "RCODE"+value if the rcode is unknown.
+// Also see [ClassToString], [TypeToString], [OpcodeToString], [CodeToString] for similar functions.
 func RcodeToString(r uint16) string {
 	if r1, ok := dns.RcodeToString[r]; ok {
 		return r1
@@ -23,6 +25,7 @@ func RcodeToString(r uint16) string {
 }
 
 // ClassToString converts the class to the text presentation, or to "CLASS"+value if the class is unknown.
+// Also see [RcodeToString], [TypeToString], [OpcodeToString], [CodeToString] for similar functions.
 func ClassToString(c uint16) string {
 	if c1, ok := dns.ClassToString[c]; ok {
 		return c1
@@ -31,6 +34,7 @@ func ClassToString(c uint16) string {
 }
 
 // OpcodeToString converts the opcode to the text presentation, or to "OPCODE"+value if the opcode is unknown.
+// Also see [RcodeToString], [TypeToString], [ClassToString], [CodeToString] for similar functions.
 func OpcodeToString(o uint8) string {
 	if o1, ok := dns.OpcodeToString[o]; ok {
 		return o1
@@ -39,6 +43,7 @@ func OpcodeToString(o uint8) string {
 }
 
 // CodeToString converts the ENDS0 code to the text presentation, or to "CODE"+value if the code is unknown.
+// Also see [RcodeToString], [TypeToString], [ClassToString], [OpcodeToString] for similar functions.
 func CodeToString(c uint16) string {
 	if c1, ok := dns.CodeToString[c]; ok {
 		return c1
