cda47372d20db9a7245d453e502334feb054ff2a
Miek Gieben <miek@miek.nl>
2026-06-25 06:58:57 +0200

    Allow VerifyFunc to work
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- dnssec.go
+++ dnssec.go
@@ -362,10 +362,7 @@ func (rr *RRSIG) Verify(k *DNSKEY, rrset []RR, options *SignOption) error {
 	sigbuf, _ := pack.Base64([]byte(rr.Signature))
 
 	var h hash.Hash
-	hash, ok := AlgorithmToHash[rr.Algorithm]
-	if !ok && rr.Algorithm != ED25519 {
-		return ErrAlg
-	}
+	hash, _ := AlgorithmToHash[rr.Algorithm] // newer alg do their own hashing, ignore if there...
 
 	switch rr.Algorithm {
 	case RSASHA1, RSASHA1NSEC3SHA1, RSASHA256, RSASHA512:
