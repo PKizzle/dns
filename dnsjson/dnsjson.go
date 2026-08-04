fe9ccc234f1b9793ba3c8eef9f8a247b8d2851ba
Miek Gieben <miek@miek.nl>
2026-08-04 22:01:01 +0200

    do not export zunpack/zpack
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- dnsjson/dnsjson.go
+++ dnsjson/dnsjson.go
@@ -1,5 +1,5 @@
 // Package dnsjson implements the RR and RRset as defined in RFC 8427. The message type is not implemented.
-// [Marshal] and [Unmarshal] are the primary interface of this package.
+// [codeberg.org/miekg/dns.MarshalJSON] and [codeberg.org/miekg/dns.UnmarshalJSON] are the primary interface of this package.
 package dnsjson
 
 // RR represents a DNS RR as specified in RFC 8427.
@@ -9,7 +9,7 @@ type RR struct {
 	TypeName  string  `json:"TYPEname,omitempty"`  // TypeName is the string representation of the type. If takes precedence of Type.
 	Type      uint16  `json:"TYPE,omitempty"`      // Type is the type of the RR.
 	ClassName string  `json:"CLASSname,omitempty"` // ClassName is the string representation of the class. It takes precedence over Class.
-	Class     uint16  `json:"CLASS,omitempty"`     // Class is the class of the RR, this is almost always [dns.ClassINET].
+	Class     uint16  `json:"CLASS,omitempty"`     // Class is the class of the RR, this is not set, class IN is assumed.
 	RdataHex  string  `json:"RDATAHEX,omitempty"`
 	RRset     []RRset `json:"rrSet,omitempty"`
 }
